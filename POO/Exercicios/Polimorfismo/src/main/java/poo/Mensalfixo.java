f17c49d3f77d381304c6c55eb4585e0f05e8b19a
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-05-30 21:48:31 -0300

    Classe Mensafixo

--- POO/Exercicios/Polimorfismo/src/main/java/poo/Mensalfixo.java
+++ POO/Exercicios/Polimorfismo/src/main/java/poo/Mensalfixo.java
@@ -6,7 +6,7 @@ public class Mensalfixo extends Funcionarios
     public Mensalfixo(String nome, String sobrenome, String numeroSeguroSocial, String cpf) {
         super(nome, sobrenome, numeroSeguroSocial, cpf);
     }
-
+//
     @Override
     public double calculaSalario() {
        return salarioFixoMensal;
